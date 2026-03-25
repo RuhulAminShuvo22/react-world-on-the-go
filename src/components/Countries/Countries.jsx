dbc3a8a87135809769de1e44218c197de1eff71d
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-03-25 17:14:53 +0600

    some thing added for the flag

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,17 +1,22 @@
 import React, { use } from 'react';
 import Country from '../Country/Country';
+import './Countries.css';
 
 const Countries = ({countriesPromise}) => {
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
     console.log(countries)
     return (
-        <div>
-            <h1>In the countries: {countries.length}</h1>
+        <div >
+            <h1>In the countries:{countries.length}</h1>
 
-            {
-                countries.map(country => <Country country = {country}></Country>)
+            <div className='countries'>
+                {
+                countries.map(country => <Country 
+                    key={country.cca3.cca3}
+                    country = {country}></Country>)
             }
+            </div>
         </div>
     );
 };
